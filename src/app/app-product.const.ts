30a366b7358bc02d5a363a80f04de10152f080eb
KenCookie <82104568+KenCookie@users.noreply.github.com>
2021-04-26 11:29:25 +0800

    甜點資料

--- src/app/app-product.const.ts
+++ src/app/app-product.const.ts
@@ -1,18 +1,156 @@
 export const products = [
     {
+      id:1,
       type: 'today',
       name: '焦糖馬卡龍',
       price: 450,
-      img: "/assets/images/others/brenda-godinez-228182-unsplash.jpg"
+      img: "/assets/images/others/01_CaramelMacaron.jpg"
     },
     {
+      id:2,
       type: 'popular',
       name: '巧克力熔岩',
-      price: 400
+      price: 400,
+      img: "/assets/images/others/02_ChocolateLava.jpg"
     },
     {
+      id:3,
       type: 'new',
       name: '提拉米蘇',
-      price: 350
+      price: 350,
+      img: "/assets/images/others/03_Tiramisu.jpg"
+    },
+    {
+      id:4,
+      type: 'new',
+      name: '奇異果藍莓優格',
+      price: 350,
+      img: "/assets/images/others/04_KiwiBlueberryYogurt.jpg"
+    },
+    {
+      id:5,
+      type: 'new',
+      name: '巧克力粉蛋糕',
+      price: 350,
+      img: "/assets/images/others/05_ChocolateFlourCake.jpg"
+    },
+    {
+      id:6,
+      type: 'new',
+      name: '草莓蛋糕',
+      price: 350,
+      img: "/assets/images/others/06_StrawberryCake.jpg"
+    },
+    {
+      id:7,
+      type: 'new',
+      name: '莓果派',
+      price: 350,
+      img: "/assets/images/others/07_BerryPie.jpg"
+    },
+    {
+      id:8,
+      type: 'new',
+      name: '草莓冰淇淋',
+      price: 350,
+      img: "/assets/images/others/08_Strawberryicecream.jpg"
+    },
+    {
+      id:9,
+      type: 'new',
+      name: '咖啡馬卡龍',
+      price: 350,
+      img: "/assets/images/others/09_CoffeeMacarons.jpg"
+    },
+    {
+      id:10,
+      type: 'new',
+      name: '白糖甜甜圈',
+      price: 350,
+      img: "/assets/images/others/10_Sugardonuts.jpg"
+    },
+    {
+      id:11,
+      type: 'today',
+      name: '牛奶糖衣甜甜圈',
+      price: 450,
+      img: "/assets/images/others/11_Milkicingdonuts.jpg"
+    },
+    {
+      id:12,
+      type: 'popular',
+      name: '重乳酪甜甜圈',
+      price: 400,
+      img: "/assets/images/others/12_Heavycheesedonuts.jpg"
+    },
+    {
+      id:13,
+      type: 'new',
+      name: '百香梅果奶酪',
+      price: 350,
+      img: "/assets/images/others/13_PassionPlumCheese.jpg"
+    },
+    {
+      id:14,
+      type: 'new',
+      name: '條紋甜甜圈',
+      price: 350,
+      img: "/assets/images/others/14_Stripeddonut.jpg"
+    },
+    {
+      id:15,
+      type: 'new',
+      name: '馬鈴薯泡芙',
+      price: 350,
+      img: "/assets/images/others/15_PotatoPuffs.jpg"
+    },
+    {
+      id:16,
+      type: 'new',
+      name: '櫻桃玫瑰甜筒',
+      price: 350,
+      img: "/assets/images/others/16_Cherryrosecone.jpg"
+    },
+    {
+      id:17,
+      type: 'new',
+      name: '莓果奶油蛋糕',
+      price: 350,
+      img: "/assets/images/others/17_BerryCreamCake.jpg"
+    },
+    {
+      id:18,
+      type: 'new',
+      name: '咖啡冰棒',
+      price: 350,
+      img: "/assets/images/others/18_Coffeepopsicle.jpg"
+    },
+    {
+      id:19,
+      type: 'new',
+      name: '果實冰棒',
+      price: 350,
+      img: "/assets/images/others/19_Fruitpopsicle.jpg"
+    },
+    {
+      id:20,
+      type: 'new',
+      name: '杏仁果鬆餅',
+      price: 350,
+      img: "/assets/images/others/20_AlmondNutMuffins.jpg"
+    },
+    {
+      id:21,
+      type: 'new',
+      name: '棉花糖瑪奇朵',
+      price: 350,
+      img: "/assets/images/others/21_MarshmallowMacchiato.jpg"
+    },
+    {
+      id:22,
+      type: 'new',
+      name: 'The_神秘糕點',
+      price: 350,
+      img: "/assets/images/others/22_MysteryPastry.jpg"
     }
   ];
